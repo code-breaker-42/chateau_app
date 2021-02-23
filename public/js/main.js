df9bcb513f51ebc4634ce9be18f7562da8414c6d
Mrinal Jha <31356384+code-breaker-42@users.noreply.github.com>
2021-02-23 16:51:28 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,66 @@
-// const socketio = require('socket.io');
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+//Get username and room from URL
+const { username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
 
-// socket.on('message', message => {
-//     console.log(message);
-// });
+//Join chatroom
+socket.emit('joinroom', { username, room});
+
+//Get room and users info
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+//Message from server, all message getting caught here and being displayed in frontend
+socket.on('message', message => {
+         console.log(message);
+         outputMessage(message);
+
+         //Scroll down
+         chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get message text
+    const msg = e.target.elements.msg.value;
+
+     //Emit a message to server
+    socket.emit('chatMessage', msg);
+
+    //Clear chat input
+    e.target.elements.msg.value = '';
+    e.target.elements.focus();
+});
+
+//Output message to DOM
+function outputMessage(message){
+    const div=document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+//Add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
+}
+
+//Add users to DOM
+function outputUsers(users){
+    userList.innerHTML = 
+    `${users.map(user => `<li>${user.username}</li>`).join('')}`;
+}
