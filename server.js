515e9b78a1962a48d56956f5a7c6d98aadbee50d
Mrinal Jha <31356384+code-breaker-42@users.noreply.github.com>
2021-02-23 16:50:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const http = require ('http');
 const socketio = require ('socket.io');
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeaves, getRoomUsers} = require('./utils/users')
 
 const app = express();
 const server =http.createServer(app);
@@ -10,10 +12,49 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'JARVIS(Bot)';
+
 // RUn when client connects
 io.on('connection', socket => { 
-    console.log('New WS connection.....');
-    socket.emit('message', 'Welcome to Chateau');
+socket.on('joinroom', ({username, room}) => {
+    const user= userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    //Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to Chateau'));
+
+    //Broadcast when a user connects
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+    //Send users and room info
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    });
+});
+
+    //Listen for chat message
+    socket.on('chatMessage', (msg) =>
+    {   
+        const user = getCurrentUser(socket.id)
+        //to send to consolelog
+        //console.log(msg);
+
+        // to send to every client and server
+        io.to(user.room).emit('message',formatMessage(user.username, msg));
+    });
+    //Runs when a client disconnects
+    socket.on('disconnect', ()=>{
+        const user = userLeaves(socket.id);
+        if(user){
+        io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+    //Send users and room info
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    });
+    }
+});
 });
 
 const PORT = process.env.PORT || 3000;
